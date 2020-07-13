6f4588cdc7da36df2a6b62d7d0590002249ed552
dci <wafi.amer.17@gmail.com>
2020-07-13 15:21:28 +0200

    Updates

---
+++ precache-manifest.3cff3a8da903f2847eb79ad0af433d9f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "961521a4d5ba1fe150ec01f8b9239479",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c111687edef09ab6bda",
+    "url": "/static/css/main.5be49278.chunk.css"
+  },
+  {
+    "revision": "bdc12fa4c520a5770e0e",
+    "url": "/static/js/2.6cd27c6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6cd27c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c111687edef09ab6bda",
+    "url": "/static/js/main.021513d6.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "b4e5bf4bf1793294dbbd988238c3a9bd",
+    "url": "/static/media/logo4.b4e5bf4b.png"
+  },
+  {
+    "revision": "96bd029f114c4a48a8af6453ca6a22aa",
+    "url": "/static/media/movie.96bd029f.png"
+  },
+  {
+    "revision": "6fae6d6dded65e201238bea104a98948",
+    "url": "/static/media/wafi1.6fae6d6d.jpg"
+  }
+]);
